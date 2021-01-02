4bb7e36f39b789d66248da969470d0627512be01
Aidan Duffy <aduffy6144@gmail.com>
2021-01-02 13:06:11 -0500

    Just creating the initial main file

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Author: Aidan Duffy
+Creation Date: January 2, 2021
+Last Updated: January 2, 2021
+Description: This program aids swimmers in scheduling/booking lap swim times
+for local YMCAs in the northern New Jersey area.
+"""
+
+def main():
+    print("test")
+
+if __name__ == '__main__':
+    main()
